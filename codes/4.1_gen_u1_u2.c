b759eea2dbde400e3c5882ea5e456778f5653eda
shreyaswankhede12 <102684318+shreyaswankhede12@users.noreply.github.com>
2022-07-03 22:18:09 +0530

    Add files via upload

---
+++ codes/4.1_gen_u1_u2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+uniform("U1.dat", 1000000);
+
+//Uniform random numbers
+ uniform("U2.dat", 1000000);
+
+//Mean of uniform
+//printf("%lf",mean("uni.dat"));
+return 0;
+}
